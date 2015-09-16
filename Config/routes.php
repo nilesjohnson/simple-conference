4e78c1ad61225474b4c0e75c37973ea16a1f54e4
Niles Johnson <nilesj@gmail.com>
2015-09-16 08:00:10 -0600

    fix routes

--- Config/routes.php
+++ Config/routes.php
@@ -24,7 +24,7 @@
  * its action called 'display', and we pass a param to select the view file
  * to use (in this case, /app/View/Pages/home.ctp)...
  */
-	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
+        Router::connect('/', array('controller' => 'registrants', 'action' => 'index'));
 /**
  * ...and connect the rest of 'Pages' controller's URLs.
  */
@@ -41,3 +41,12 @@
  * the built-in default routes.
  */
 	require CAKE . 'Config' . DS . 'routes.php';
+
+
+
+
+/**
+ * activate rss
+ */
+
+	Router::parseExtensions('rss','ics');
